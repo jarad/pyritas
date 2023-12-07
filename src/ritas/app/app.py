0ae33f09ab72a102ecdb4440871f4678ad2c498d
benthecoder <benthecoder07@gmail.com>
2023-12-06 18:31:02 -0600

    Update app.py to include other functions

--- src/ritas/app/app.py
+++ src/ritas/app/app.py
@@ -1,8 +1,9 @@
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
-from ritas.polygons import make_vehicle_polygons
+from ritas.polygons import chop_polygons, make_grid, make_vehicle_polygons, reshape_polygons
 from ritas.viz import plot_coordinates, plot_map
 
 
@@ -40,6 +41,38 @@ def main() -> None:
                 st.pyplot(fig)
 
             st.header("Polygons (After reshaping)")
+            with st.spinner("Reshaping polygons..."):
+                polygons = reshape_polygons(polygons)
+                fig = plot_map(polygons)
+                st.pyplot(fig)
+
+            # grid
+            st.header("Grid")
+            with st.spinner("Creating grid..."):
+                grid = make_grid(reshaped_geodf, width=5, height=5)
+                fig, ax = plt.subplots(figsize=(20, 10))
+                _, ax = plot_map(grid, ax=ax)
+                fig, ax = plot_map(reshaped_geodf, ax=ax)
+                st.pyplot(fig)
+
+            with st.spinner("Chopping grid..."):
+                col_identity = [col for col in reshaped_geodf.columns if col not in ["x", "y"]]
+                col_identity.append("effectiveArea")
+                col_weight = ["mass", "effectiveArea"]
+
+                chopped = chop_polygons(reshaped_geodf, grid, col_identity, col_weight)
+                fig = plot_map(chopped)
+                st.pyplot(fig)
+
+            with st.spinner("Aggregating..."):
+                col_weight = ["mass", "effectiveArea"]
+                col_names = [f"{col}W" for col in col_weight]
+                col_funcs = [np.sum, np.sum]
+
+                agg_df = aggregate_polygons(chopped, grid, col_names, col_funcs, by=["gridPolyID"])
+
+                fig = plot_map(agg_df, column="massWUp")
+                st.pyplot(fig)
 
 
 if __name__ == "__main__":
