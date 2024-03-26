9fe238e4a3382c753a7ae49b7fa6a9f8394cc8cb
akrherz <akrherz@iastate.edu>
2024-03-26 08:40:19 -0500

    fix: typo with module name

--- src/ritas/__init__.py
+++ src/ritas/__init__.py
@@ -5,7 +5,7 @@ from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 
 try:
-    __version__ = version("pyiem")
+    __version__ = version("ritas")
     pkgdir = Path(os.path.realpath(__file__)).parent.parent
     if not str(pkgdir).endswith("site-packages"):
         __version__ += "-dev"
